976be6e2f02fccda6d170f9d4872a23ca6b400b9
tienthienngt1 <tienthienngt2@gmail.com>
2023-01-05 07:21:58 +0700

    get item complete

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+require("dotenv").config();
 
-app.get("/",async (req,res)=>{
-  const re = await axios.get("https://shopee.vn/api/v4/item/get?itemid=17689791830&shopid=711707322",{headers:{"af-ac-enc-dat":"null"}})
-  res.json(200, {data: re.data})
-})
+app.get("", (_req, res) => {
+	res.send("hello world");
+});
+
+app.get("/item/get", async (req, res) => {
+	const response = await axios.get(
+		`${process.env.BASE_API_URL}item/get?itemid=${req.query.itemid}&shopid=${req.query.shopid}`,
+		{ headers: { "af-ac-enc-dat": "null" } }
+	);
+	res.json(200, { data: response.data });
+});
 
 app.listen(3000);
