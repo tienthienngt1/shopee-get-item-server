7c8b74bddee85eeaeaf5eef84f6ae69960cc9bdc
tienthienngt1 <tienthienngt2@gmail.com>
2023-01-05 08:24:44 +0700

    set cors

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const cors = require("cors");
 require("dotenv").config();
+
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+};
+
+app.use(cors(corsOptions));
 
 app.get("", (_req, res) => {
 	res.send("hello world");
